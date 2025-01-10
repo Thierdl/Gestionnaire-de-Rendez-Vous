98ad3c4cbb24a899d374807a06ab62a81461f34a
thierdl <thierdl70@gmail.com>
2025-01-10 16:53:43 +0000

    add function delete patient

--- project/patient/views.py
+++ project/patient/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, get_object_or_404
 from .import models
 # Create your views here.
 
@@ -27,6 +27,24 @@ def addpatient(request):
     return render(request, "patient/addpatient.html")
 
 def update(request):
+    patient=get_object_or_404(models.Patient)
+    if request.method=="POST":
+
+        name=request.POST.get("name")
+        firstname=request.POST.get("firstname")
+        age=request.POST.get("age")
+        adress=request.POST.get("adress")
+        phone=request.POST.get("phone")
+
+        patient.name=name
+        patient.firstname=firstname
+        patient.age=age
+        patient.adress=adress
+        patient.phone=phone
+
+        patient.delete()
+        return redirect()
+
     return render(request)
 
 def delete(request):
