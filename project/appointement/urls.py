73cf4756b104e9d3941822f3cb610270a217ef68
thierdl <thierdl70@gmail.com>
2025-01-14 21:55:47 +0000

    add dashboard

--- project/appointement/urls.py
+++ project/appointement/urls.py
@@ -4,7 +4,7 @@ from appointement import views
 
 urlpatterns = [
     
-    path('', views.index_views, name="index"),
+    path('index', views.index_views, name="index"),
     
     path('test',views.testhtml,  name="test"),
     path('add', views.add_appointement, name="add"),
