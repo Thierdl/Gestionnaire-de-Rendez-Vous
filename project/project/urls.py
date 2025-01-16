a0f6e5056432029cbe045d20628b57d207b7085b
thierdl <thierdl70@gmail.com>
2025-01-16 16:58:08 +0000

    improve dashboard end create template index

--- project/project/urls.py
+++ project/project/urls.py
@@ -1,5 +1,6 @@
 
 from django.contrib import admin
+from django.conf.urls.static import static
 from django.urls import path, include
 from rest_framework import routers
 from api.views import AppointementViewSet, PatientViewSet
@@ -22,9 +23,18 @@ urlpatterns = [
 ]
 
 
+if settings.DEBUG:
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
+
+
 
 if settings.DEBUG:
     import debug_toolbar
     urlpatterns += [
         path('__debug__/', include(debug_toolbar.urls)),
     ]
+
+
