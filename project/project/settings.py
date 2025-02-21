d2b7f9581409b6b884a6869243229fc75087698e
thierdl <hthierdl70@gmail.com>
2025-02-21 07:27:16 +0000

    test

--- project/project/settings.py
+++ project/project/settings.py
@@ -162,20 +162,15 @@ AUTH_PASSWORD_VALIDATORS = [
 
 
 
-
-# -----------------------------------------------------------
-# settings.py
-
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.sendgrid.net'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
 EMAIL_HOST_USER = 'apikey'  
-EMAIL_HOST_PASSWORD = config('SENDGRID_API_KEY') #
+EMAIL_HOST_PASSWORD = config('SENDGRID_API_KEY') 
 DEFAULT_FROM_EMAIL = 'hthierdl70@gmail.com'  
 
 
-# -----------------------------------------------------------
 
 
 
